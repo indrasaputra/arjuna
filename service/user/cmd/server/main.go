2b8bc1afbc0ced2bc5e3beaca5938cb5a8fb734b
Indra Saputra <flame.fire99@gmail.com>
2024-08-31 17:47:37 +0700

    chore: seed as subcommand (#53)
    * chore: move seeder to cmd/server
    
    * chore: update compose.yml
    
    * fix: rename json file
    
    * fix: add seed subcommand in wallet
    
    * chore: update seed script
    
    * chore: delete seeder dir

--- service/user/cmd/server/main.go
+++ service/user/cmd/server/main.go
@@ -3,7 +3,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -13,10 +17,12 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
+	skdpostgres "github.com/indrasaputra/arjuna/pkg/sdk/database/postgres"
 	"github.com/indrasaputra/arjuna/pkg/sdk/grpc/server"
 	sdklog "github.com/indrasaputra/arjuna/pkg/sdk/log"
 	"github.com/indrasaputra/arjuna/pkg/sdk/trace"
 	apiv1 "github.com/indrasaputra/arjuna/proto/api/v1"
+	"github.com/indrasaputra/arjuna/service/user/entity"
 	"github.com/indrasaputra/arjuna/service/user/internal/app"
 	"github.com/indrasaputra/arjuna/service/user/internal/builder"
 	"github.com/indrasaputra/arjuna/service/user/internal/config"
@@ -46,6 +52,11 @@ func main() {
 		Use:   "relayer",
 		Short: "Run the relayer.",
 		Run:   Relayer,
+	})
+	command.AddCommand(&cobra.Command{
+		Use:   "seed",
+		Short: "Run the seeder.",
+		Run:   Seed,
 	})
 
 	if err := command.Execute(); err != nil {
@@ -170,6 +181,20 @@ func Relayer(_ *cobra.Command, _ []string) {
 	}
 }
 
+// Seed is the entry point for running the seeder.
+func Seed(_ *cobra.Command, _ []string) {
+	ctx := context.Background()
+
+	cfg, err := config.NewConfig(".env")
+	checkError(err)
+	db, err := builder.BuildBunDB(cfg.Postgres)
+	checkError(err)
+
+	val := openJSON("test/fixture/users.json")
+
+	insertUsers(ctx, db, val)
+}
+
 func registerGrpcService(srv *server.Server, dep *builder.Dependency) {
 	// start register all module's gRPC handlers
 	command := builder.BuildUserCommandHandler(dep)
@@ -184,6 +209,31 @@ func registerGrpcService(srv *server.Server, dep *builder.Dependency) {
 		grpc_health_v1.RegisterHealthServer(server, health)
 	})
 	// end of register all module's gRPC handlers
+}
+
+func openJSON(file string) []byte {
+	jsonFile, err := os.Open(filepath.Clean(file))
+	checkError(err)
+	defer func() {
+		_ = jsonFile.Close()
+	}()
+
+	val, err := io.ReadAll(jsonFile)
+	checkError(err)
+
+	return val
+}
+
+func insertUsers(ctx context.Context, db *skdpostgres.BunDB, val []byte) {
+	var users []*entity.User
+	_ = json.Unmarshal(val, &users)
+
+	query := "INSERT INTO users (id, name, created_at, updated_at) VALUES (?, ?, NOW(), NOW())"
+	for _, user := range users {
+		_, err := db.Exec(ctx, query, user.ID, user.Name)
+		checkError(err)
+	}
+	log.Printf("Successfully insert %d users\n", len(users))
 }
 
 func checkError(err error) {
--- tool/script/seed.sh
+++ tool/script/seed.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-CMD=seeder
+CMD=server
 APP_DIR=service/$1
 
 if [ -d ${APP_DIR}/cmd/${CMD} ]; then
     echo "running seeder for ${APP_DIR}..."
     (cd ${APP_DIR} && \
-        go run cmd/${CMD}/main.go)
+        go run cmd/${CMD}/main.go seed)
 fi
