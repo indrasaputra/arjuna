cefdaca977511fdf27ca7176c498adafe84364ac
Indra Saputra <flame.fire99@gmail.com>
2024-09-25 20:28:35 +0700

    chore: use build tag for integration test (#55)
    * chore: add vscode local setting
    
    * test: add build tag integration
    
    * chore: update test script
    
    * chore: remove .vscode and add integration tags in goimports
    
    * chore: upgrade clang-format-lint to v0.18.2
    
    * refactor: use coderabbitai suggestions
    
    * deps: upgrade postgres to 16.4

--- tool/script/test.sh
+++ tool/script/test.sh
@@ -2,35 +2,35 @@
 
 set -eo pipefail
 
+run_tests() {
+    go clean -testcache
+    go test -count=1 -failfast -v -race ./...
+}
+
+run_tests_cover() {
+    go clean -testcache
+    go test -count=1 -failfast -v -race -coverprofile=coverage.out $(go list ./... | grep -v /test/)
+    go tool cover -html=coverage.out -o coverage.html
+    go tool cover -func coverage.out
+}
+
 if [[ $1 = 'cover' ]]; then
     if [ $2 ]; then
-        (cd ./$2 && 
-            go clean -testcache &&
-            go test -count=1 -failfast -v -race -coverprofile=coverage.out $(go list ./... | grep -v /test/) &&
-            go tool cover -html=coverage.out -o coverage.html &&
-            go tool cover -func coverage.out)
+        (cd ./$2 && run_tests_cover) # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
     else
-        for dir in `find . -type d`; do
+        find . -type d | while IFS= read -r dir; do # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
             if [[ -f ${dir}/go.mod ]]; then
-                (cd ${dir} && 
-                    go clean -testcache &&
-                    go test -count=1 -failfast -v -race -coverprofile=coverage.out $(go list ./... | grep -v /test/) &&
-                    go tool cover -html=coverage.out -o coverage.html &&
-                    go tool cover -func coverage.out)
+                (cd ${dir} && run_tests_cover) # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
             fi
         done
     fi
 elif [[ $1 = 'unit' ]]; then
     if [ $2 ]; then
-        (cd ./$2 && 
-            go clean -testcache &&
-            go test -count=1 -failfast -v -race $(go list ./... | grep -v /test/))
+        (cd ./$2 && run_tests) # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
     else
-        for dir in `find . -type d`; do
+        find . -type d | while IFS= read -r dir; do # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
             if [[ -f ${dir}/go.mod ]]; then
-                (cd ${dir} && 
-                    go clean -testcache &&
-                    go test -count=1 -failfast -v -race $(go list ./... | grep -v /test/))
+                (cd ${dir} && run_tests) # from https://github.com/indrasaputra/arjuna/pull/55#pullrequestreview-2328123308
             fi
         done
     fi
