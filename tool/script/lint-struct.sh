bcac652df13e6892e842540324e97f7e37a65c8e
Indra Saputra <flame.fire99@gmail.com>
2025-01-19 12:06:44 +0700

    feat: open API for wallet transfer and run fieldaligment until no issue found

--- tool/script/lint-struct.sh
+++ tool/script/lint-struct.sh
@@ -2,14 +2,28 @@
 
 set -eo pipefail
 
+run_fieldalignment() {
+    local dir=$1
+    local has_changes=1
+
+    while [[ $has_changes -ne 0 ]]; do
+        echo "running fieldalignment in ${dir}..."
+        if output=$(cd "${dir}" && fieldalignment -fix ./... 2>&1); then
+            has_changes=0
+        else
+            echo "fieldalignment detected issues, retrying..."
+            echo "$output"
+        fi
+    done
+}
+
 if [[ -n $1 ]]; then
     echo "running in $1"
     (cd $1 && fieldalignment -fix ./...)
 else
     for dir in `find . -type d`; do
         if [[ -f ${dir}/go.mod ]]; then
-            echo "running in ${dir}"
-            (cd ${dir} && fieldalignment -fix ./...)
+            run_fieldalignment "${dir}"
         fi
     done
 fi
