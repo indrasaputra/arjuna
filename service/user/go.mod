cdd9d39f7ba42d599cf6fc5b6131c84eff17ad58
Indra Saputra <flame.fire99@gmail.com>
2025-11-21 17:08:23 +0700

    chore: revert to 1.25.2

--- service/user/go.mod
+++ service/user/go.mod
@@ -1,6 +1,6 @@
 module github.com/indrasaputra/arjuna/service/user
 
-go 1.25.3
+go 1.25.2
 
 exclude google.golang.org/genproto v0.0.0-20180518175338-11a468237815
 
--- tool/script/test.sh
+++ tool/script/test.sh
@@ -2,31 +2,14 @@
 
 set -eo pipefail
 
-# In CI, limit parallelism to reduce memory pressure from race detector
-if [ "${CI}" = "true" ] || [ "${GITHUB_ACTIONS}" = "true" ]; then
-    # -p=1: run packages sequentially, -parallel=1: run tests within package sequentially
-    PARALLEL_FLAG="-p=1 -parallel=1"
-    # Enable CGO for race detector and configure TSAN for minimal memory usage
-    export CGO_ENABLED=1
-    # Ultra-aggressive TSAN options to minimize memory usage
-    export TSAN_OPTIONS="halt_on_error=1:history_size=1:io_sync=0:flush_memory_ms=0:die_after_fork=0"
-    export GORACE="halt_on_error=1"
-    # Aggressive garbage collection to free memory quickly
-    export GOGC=20
-    # Reduce Go's memory limit
-    export GOMEMLIMIT=2GiB
-else
-    PARALLEL_FLAG=""
-fi
-
 run_tests() {
     go clean -testcache
-    go test -count=1 -failfast -v -race ${PARALLEL_FLAG} ./...
+    go test -count=1 -failfast -v -race ./...
 }
 
 run_tests_cover() {
     go clean -testcache
-    go test -count=1 -failfast -v -race ${PARALLEL_FLAG} -coverprofile=coverage.out $(go list ./... | grep -v /test/)
+    go test -count=1 -failfast -v -race -coverprofile=coverage.out $(go list ./... | grep -v /test/)
     go tool cover -html=coverage.out -o coverage.html
     go tool cover -func coverage.out
 }
